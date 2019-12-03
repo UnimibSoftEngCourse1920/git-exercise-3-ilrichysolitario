46c0f67f321579958cc92517191bcc46aaa44444
Riccardo Maria Frighi <r.frighi@campus.unimib.it>
2019-12-03 10:56:27 +0100

    Merge 1

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -963,78 +963,8 @@ public class Assert {
             Matcher<? super T> matcher) {
         MatcherAssert.assertThat(reason, actual, matcher);
     }
-<<<<<<< HEAD
     
     public static <T> void assertGreaterThan (T o1, T o2, java.util.Comparator<T> comparator) {
         assertEquals(comparator.compare(o1, o2), 1);
-=======
-
-    /**
-     * Asserts that {@code runnable} throws an exception of type {@code expectedThrowable} when
-     * executed. If it does, the exception object is returned. If it does not throw an exception, an
-     * {@link AssertionError} is thrown. If it throws the wrong type of exception, an {@code
-     * AssertionError} is thrown describing the mismatch; the exception that was actually thrown can
-     * be obtained by calling {@link AssertionError#getCause}.
-     *
-     * @param expectedThrowable the expected type of the exception
-     * @param runnable       a function that is expected to throw an exception when executed
-     * @return the exception thrown by {@code runnable}
-     * @since 4.13
-     */
-    public static <T extends Throwable> T assertThrows(Class<T> expectedThrowable,
-            ThrowingRunnable runnable) {
-        return assertThrows(null, expectedThrowable, runnable);
-    }
-
-    /**
-     * Asserts that {@code runnable} throws an exception of type {@code expectedThrowable} when
-     * executed. If it does, the exception object is returned. If it does not throw an exception, an
-     * {@link AssertionError} is thrown. If it throws the wrong type of exception, an {@code
-     * AssertionError} is thrown describing the mismatch; the exception that was actually thrown can
-     * be obtained by calling {@link AssertionError#getCause}.
-     *
-     * @param message the identifying message for the {@link AssertionError} (<code>null</code>
-     * okay)
-     * @param expectedThrowable the expected type of the exception
-     * @param runnable a function that is expected to throw an exception when executed
-     * @return the exception thrown by {@code runnable}
-     * @since 4.13
-     */
-    public static <T extends Throwable> T assertThrows(String message, Class<T> expectedThrowable,
-            ThrowingRunnable runnable) {
-        try {
-            runnable.run();
-        } catch (Throwable actualThrown) {
-            if (expectedThrowable.isInstance(actualThrown)) {
-                @SuppressWarnings("unchecked") T retVal = (T) actualThrown;
-                return retVal;
-            } else {
-                String expected = formatClass(expectedThrowable);
-                Class<? extends Throwable> actualThrowable = actualThrown.getClass();
-                String actual = formatClass(actualThrowable);
-                if (expected.equals(actual)) {
-                    // There must be multiple class loaders. Add the identity hash code so the message
-                    // doesn't say "expected: java.lang.String<my.package.MyException> ..."
-                    expected += "@" + Integer.toHexString(System.identityHashCode(expectedThrowable));
-                    actual += "@" + Integer.toHexString(System.identityHashCode(actualThrowable));
-                }
-                String mismatchMessage = buildPrefix(message)
-                        + format("unexpected exception type thrown;", expected, actual);
-
-                // The AssertionError(String, Throwable) ctor is only available on JDK7.
-                AssertionError assertionError = new AssertionError(mismatchMessage);
-                assertionError.initCause(actualThrown);
-                throw assertionError;
-            }
-        }
-        String notThrownMessage = buildPrefix(message) + String
-                .format("expected %s to be thrown, but nothing was thrown",
-                        formatClass(expectedThrowable));
-        throw new AssertionError(notThrownMessage);
-    }
-
-    private static String buildPrefix(String message) {
-        return message != null && message.length() != 0 ? message + ": " : "";
->>>>>>> refs/heads/master
     }
 }
